2e42222b4687c536cafe283dd35d8acda36c8803
alan <alan@wecancodeit.org>
2021-05-23 10:09:59 -0400

    initial commit

---
+++ src/main/java/org/wecancodeit/paperplateanalysis/PaperPlateAnalysisApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.paperplateanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperPlateAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperPlateAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wecancodeit/paperplateanalysis/PaperPlateAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.paperplateanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperPlateAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
